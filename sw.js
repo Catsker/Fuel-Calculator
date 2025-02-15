0b0be967a1bfdd5d8605334b5bb74688aaccbe1c
Pavel Kazarenko <pkasarenko@gmail.com>
2025-02-15 13:40:22 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'fuel-app-cache-v2.2.2';
+const staticCacheName = 'fuel-app-cache-v2.2.3';
 
 const assetUrls = [
     '/',                    // Корневой путь для index.html
